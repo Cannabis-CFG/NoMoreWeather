f7909f02b6a778334637c200a7f556b52c531a9f
Cannabis-CFG <33133912+Cannabis-CFG@users.noreply.github.com>
2021-12-08 07:20:58 -0600

    Initial commit

---
+++ NoMoreWeather.cs
@@ -0,0 +1,41 @@
+﻿using EFT.Weather;
+using MelonLoader;
+
+namespace NoMoreWeather
+{
+    public class NoMoreWeather : MelonMod
+    {
+        bool inRaid = false;
+
+
+        public override void OnUpdate()
+        {
+            if (inRaid) 
+            {
+                WeatherController.Instance.RainController.SetIntensity(0f);
+                WeatherController.Instance.GlobalFogOvercast = 0f;
+                WeatherController.Instance.LightningSummonBandWidth = 0f;
+                WeatherController.Instance.RainController.enabled = false;
+            }
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            // Check the game scene's name to see if player is in raid
+            switch (sceneName)
+            {
+                // In raid
+                case "GameUIScene":
+                    inRaid = true;
+                    break;
+                // Not in raid
+                case "SessionEndUIScene":
+                    inRaid = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NoMoreWeather")]
+[assembly: AssemblyDescription("no. More. Weather.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NoMoreWeather")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: MelonInfo(typeof(NoMoreWeather.NoMoreWeather), "NoMoreWeather", "1.0.0", "Cannabis")]
+[assembly: MelonGame("Battlestate Games", "Escape from Tarkov")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("995a593e-20f7-4bbd-a9ee-d4ccf093bf61")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
